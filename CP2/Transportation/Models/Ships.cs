8020411f3bb6ba5d1206d99acde053bd23e3473e
JazminPamelaMontenegroBaltodano <jmontenegro00328@ufide.ac.cr>
2025-12-02 19:26:31 -0600

    CP2 completado
    CP2 completado

---
+++ CP2/Transportation/Models/Ships.cs
@@ -0,0 +1,20 @@
+namespace Transportation.Models;
+
+public class Ships
+{
+    public DateTime EndOfTitanic()
+    {
+        // Fecha de hundimiento del Titanic.
+        return new DateTime(1912, 4, 15);
+    }
+
+    public DateTime EndOfBritannic()
+    {
+        return new DateTime(1916, 11, 21);
+    }
+
+    public DateTime EndOfOlympic()
+    {
+        return new DateTime(1935, 4, 12);
+    }
+}
